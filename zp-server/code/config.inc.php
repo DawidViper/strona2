77f102456c72671da6a6d0ebaa0cd15aa1b6aa1d
dawid <43915048+DawidViper@users.noreply.github.com>
2018-11-10 13:16:46 +0100

    Add files via upload

---
+++ zp-server/code/config.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+define('ZPS_API_KEY', '979e572fa8cb4c8b9d2ac92ce7cc6777'); // Hier wird von Zeta Producer der API-Key reingeschrieben.
+define('ZPS_VERSION', '14.3.0.0');
+define('ZPS_DEBUG', false);
+
+?>
